5106ff1a62179fa45f92c45305d48beb9e77b11c
Joel Waage and Russ Davies <joel+russ@>
2016-09-01 15:24:15 -0700

    create city tile and move delete funtion to city-tile

--- app/routes/index.js
+++ app/routes/index.js
@@ -7,11 +7,15 @@ export default Ember.Route.extend({
       rentals: this.store.findAll('rental')
     });
   },
-
   actions: {
     save3(params) {
       var newRental = this.store.createRecord('rental', params);
       newRental.save();
+      this.transitionTo('index');
+    },
+    saveCity(params) {
+      var newCity = this.store.createRecord('city', params);
+      newCity.save();
       this.transitionTo('index');
     }
   }
